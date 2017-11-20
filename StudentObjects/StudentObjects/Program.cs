75f899452bd8e4ef77dd05ca5ea40d178bd93d1d
Hugo Robinson <hugogbrobinson@gmail.com>
2017-11-20 09:55:52 +0000

    Added the Student Records text file and the first itereation of the Student Objects

--- StudentObjects/StudentObjects/Program.cs
+++ StudentObjects/StudentObjects/Program.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace StudentObjects
 {
     class Program
     {
+        static List<Student> records = new List<Student>();
         static void Main(string[] args)
         {
+            ReadToStudentList();
+            
+            
+        }
+
+        private static void ReadToStudentList()
+        {
+            Console.Write("Enter FileName: ");
+            string fileName = Console.ReadLine() + ".txt";
+            try
+            {
+                ReadFileIntoElementsArray(fileName);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File does not exist");
+            }
+
+
+
+        }
+        static void ReadFileIntoElementsArray(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                var ElementsArray = reader.ToString().Split(',');
+
+
+            }
+        }
+
+        private static void PrintStudent(Student s1)
+        {
+            Console.WriteLine(s1.Number);
+            Console.WriteLine(s1.FirstName);
+            Console.WriteLine(s1.LastName);
+            Console.WriteLine(s1.DateOfBirth);
+            Console.WriteLine(s1.Grade);
+            Console.ReadKey();
         }
     }
 }
---
+++ StudentObjects/StudentObjects/StudentMedical.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentObjects
+{
+    class StudentMedical
+    {
+        public double Hight { get; set; }
+
+        public double Weight { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
